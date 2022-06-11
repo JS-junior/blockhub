905f3de4f6c11ce35dafa3617997e8158022f9b9
JS-junior <arnabgogoi83@gmail.com>
2022-06-11 10:37:31 +0530

    hello world

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
+import React from "react"
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from "./components/home"
+import Settings from "./components/settings"
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Settings" component={Settings} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Random bullshit go!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
   );
 }
 
---
+++ components/settings.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useLayoutEffect, useRef  } from "react"
+
+import { Text, View, Image, StyleSheet,FlatList, TouchableOpacity, ScrollView, Platform, Linking, ToastAndroid, Switch } from "react-native"
+
+import styles from "./styles"
+
+export default function Settings(){
+    return(
+          <View style={styles.container}>
+            <Text>Settings screen </Text>
+          </View>
+          )
+}
